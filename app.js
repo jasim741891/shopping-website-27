98e6a2c1f9315086bdbad24c59409574b29defb4
jasim741891 <jasimgai74@gmail.com>
2021-08-18 11:39:10 +0600

    first js code complet

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('I am in a separate file')
+function updateProductNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
+    if (isIncreasing == true) {
+        productNumber = parseInt(productNumber) + 1;
+    }
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
+    }
+    productInput.value = productNumber;
+    // update case total
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
+}
+// phone increase and decrese
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, true);
+});
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, false);
+})
+
+
+// handle case increase and decrease event 
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateProductNumber('case', 59, true);
+});
+
+
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateProductNumber('case', 59, false);
+
+})
